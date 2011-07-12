f3cbadbd36a114f5d5705dc7b45474d8e2ae1e25
hogehoge <hogehoge@dce7a640-d69e-4be9-95f4-2b15bb2ce525>
2011-07-12 19:30:51 +0000

    fixed some of negligences around svn
    git-svn-id: svn://localhost@41 dce7a640-d69e-4be9-95f4-2b15bb2ce525

---
+++ branches/reshape_alpha/conf/conf_mail.m
@@ -0,0 +1,15 @@
+if isfield(env,'mail')
+  status.mail = 1; % 0: don't notify with mail when program ended.
+else
+  status.mail = 0;
+end
+%if ~exist('env')
+if exist('env') && ~isfield(env ,'mail')
+  %% Set YOUR_MAIL_ADDRESS your mail address to which
+  %% finish of program is notified 
+  env.mail.to='YOUR_MAIL_ADDRESS';
+  if ~isfield(env.mail,'smpt')
+    %% Set your 'YOUR_SMTP_SERVER'
+    env.mail.smtp='YOUR_SMTP_SERVER';
+  end
+end
