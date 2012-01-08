4508c3fefcdc146d42e2631a775ddc88915fa923
hogehoge <hogehoge@dce7a640-d69e-4be9-95f4-2b15bb2ce525>
2012-01-08 08:18:17 +0000

    tirivial fix
    git-svn-id: svn://localhost@117 dce7a640-d69e-4be9-95f4-2b15bb2ce525

--- branches/reshape_alpha/mylib/check/check_gendI.m
+++ branches/reshape_alpha/mylib/check/check_gendI.m
@@ -3,7 +3,7 @@
 tmpPrm = 1000;
 tmpNidx = 1:env.cnum;
 
-status.usedNeruronIdx  = tmpNidx.*(sum(Iorg,1) > tmpPrm)
+status.usedNeruronIdx  = tmpNidx.*(sum(Iorg,1) > tmpPrm);
 I = Iorg(:,(status.usedNeruronIdx>0));
 env.inFiringUSE = size(I,2);
 %status.usedNeruronIdx =status.usedNeruronIdx(status.usedNeruronIdx
